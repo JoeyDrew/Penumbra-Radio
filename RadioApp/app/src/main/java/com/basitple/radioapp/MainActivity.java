eaccd7fa8abceed62e0a0d6b155f0319e6a9d0b1
Jonathan Houston <jonhouston2011@gmail.com>
2017-12-05 22:26:15 -0600

    First Commit from Desktop

--- RadioApp/app/src/main/java/com/basitple/radioapp/MainActivity.java
+++ RadioApp/app/src/main/java/com/basitple/radioapp/MainActivity.java
@@ -9,11 +9,13 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
@@ -45,5 +47,20 @@ public void register(View view){
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    //Method to Hiding Navigation Bar
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
     }
 }
--- RadioApp/app/src/main/java/com/basitple/radioapp/Main_page2.java
+++ RadioApp/app/src/main/java/com/basitple/radioapp/Main_page2.java
@@ -1,5 +1,8 @@
 package com.basitple.radioapp;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,21 +11,50 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class Main_page2 extends AppCompatActivity {
+public class Main_page2 extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page2);
 
 
 
     }
-public void alarm(View view){
-    Intent i = new Intent(this,MainPage.class);
-    startActivity(i);
-}
+    public void alarm(View view)
+    {
+        loadFragment(new alarm());
+    }
 
+    //Method to Hiding Navigation Bar
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
+    }
 
+    //FragmentManager(fm) swaps out the fragments
+    private void loadFragment(Fragment fragment)
+    {
+        // create a FragmentManager
+        FragmentManager fm = getFragmentManager();
 
+        // create a FragmentTransaction to begin the transaction and replace the Fragment
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+
+        // replace the FrameLayout with new Fragment
+        //fragmentTransaction.replace(R.id., fragment); //needs a home
+        fragmentTransaction.commit(); // save the changes
+    }
 }
