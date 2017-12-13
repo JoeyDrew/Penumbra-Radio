b1dcd506d947332dc4e5ab942e2976de5feafb82
basit <basit@DESKTOP-SNUA689>
2017-12-13 13:39:20 -0600

    work on registration with checking if account already exit.... not completed but works,

--- RadioApp/app/src/main/java/com/basitple/radioapp/MainRegistrationActivity.java
+++ RadioApp/app/src/main/java/com/basitple/radioapp/MainRegistrationActivity.java
@@ -17,11 +17,13 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.io.Writer;
+  import java.io.UnsupportedEncodingException;
+  import java.io.Writer;
 import java.net.HttpRetryException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
-import java.net.URL;
+  import java.net.ProtocolException;
+  import java.net.URL;
 
     public class MainRegistrationActivity extends AppCompatActivity {
 
@@ -45,7 +47,6 @@ import java.net.URL;
                 public void onClick(View v) {
                     if (editEmail.getText().toString().equals(editEmailRe.getText().toString())) {
                         senddatatoserver(v);
-                        finish();
                         Log.e("reply", "added");
                     } else {
                         editEmail.setText("");
@@ -73,7 +74,7 @@ import java.net.URL;
                 e.printStackTrace();
             }
             if (new_user.length() > 0) {
-                new sendJsonDataToServer().execute(String.valueOf(wrap));
+                new sendJsonDataToServer().execute(String.valueOf(wrap), editEmailText);
             }
         }
     }
@@ -82,6 +83,8 @@ import java.net.URL;
         protected String doInBackground(String... params) {
             String JsonResponse = null;
             String JsonDATA = params[0];
+            String userEmail = params[1];
+            boolean exist = false;
             HttpURLConnection urlConnection = null;
             BufferedReader reader = null;
             try {
@@ -90,39 +93,71 @@ import java.net.URL;
                 urlConnection = (HttpURLConnection) url.openConnection();
                 // urlConnection.setDoOutput(true);
                 // is output buffer writter
-                urlConnection.setRequestMethod("POST");
-                urlConnection.setRequestProperty("Content-Type", "application/json");
                 urlConnection.connect();
-                //urlConnection.setRequestProperty("Accept", "application/json");
-//set headers and method
-                Writer writer = new BufferedWriter(new OutputStreamWriter(urlConnection.getOutputStream(), "UTF-8"));
-                writer.write(JsonDATA);
-// json data
-                writer.close();
-                InputStream inputStream = urlConnection.getInputStream();
-//input stream
+                //grab input stream from server.
+                InputStream stream = urlConnection.getInputStream();
+                reader = new BufferedReader(new InputStreamReader(stream));
+                String line = "";
                 StringBuffer buffer = new StringBuffer();
-                // reader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
-                Log.e("onject", JsonDATA);
-                //String inputLine;
-                //while ((inputLine = reader.readLine()) != null)
-                //  buffer.append(inputLine + "\n");
-                //if (buffer.length() == 0) {
-                // Stream was empty. No point in parsing.
-                //  return null;
-                //}
-                JsonResponse = buffer.toString();
+                //read all input Stream.
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line);
+                }
+                // convert input Stream to string
+                String finalJson = buffer.toString();
+                /// make a jsonObject
+                JSONObject parentOnject = new JSONObject(finalJson);
+                ///grab the array in json:users
+                JSONArray users = parentOnject.getJSONArray("users");
+                JSONObject user;
+                String welcome = "";
+
+                //iterate through the list to get email and id.
+                for (int i = 0; i < users.length(); i++) {
+                    user = users.getJSONObject(i);
+                    if (userEmail.equalsIgnoreCase(user.getString("email"))) {
+                        exist = true;
+                        Log.e("msg", "user exit");
+                        break;
+                    } else {
+                    }
+                    // Log.e("email", user.getString("email").toString());
+                }
+                if (exist == false) {
+
+                    HttpURLConnection urlConnection2 = (HttpURLConnection)  url.openConnection();
+                    urlConnection2.setRequestMethod("POST");
+                    urlConnection2.setRequestProperty("Content-Type", "application/json");
+                    urlConnection2.connect();
+                    //urlConnection.setRequestProperty("Accept", "application/json");
+//set headers and method
+                    Writer writer = new BufferedWriter(new OutputStreamWriter(urlConnection2.getOutputStream(), "UTF-8"));
+                    writer.write(JsonDATA);
+// json data
+                    writer.close();
+                    InputStream inputStream = urlConnection2.getInputStream();
+//input stream
+                    //StringBuffer buffer = new StringBuffer();
+                    // reader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                    Log.e("onject", JsonDATA);
+                    //String inputLine;
+                    //while ((inputLine = reader.readLine()) != null)
+                    //  buffer.append(inputLine + "\n");
+                    //if (buffer.length() == 0) {
+                    // Stream was empty. No point in parsing.
+                    //  return null;
+                    //}
+                    JsonResponse = buffer.toString();
 //response data
-                Log.i("server:", "hello");
-//send to post execute
-                return "hello";
-                //JsonResponse;
-
-            } catch (MalformedURLException e) {
-
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
+                    Log.i("server:", "hello");
+//send to post execute}catch(MalformedURLException e){
+                }
+            } catch (ProtocolException e1) {
+                e1.printStackTrace();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            } catch (JSONException e1) {
+                e1.printStackTrace();
             } finally {
                 if (urlConnection != null) {
                     urlConnection.disconnect();
@@ -134,8 +169,10 @@ import java.net.URL;
                         Log.e("ERROR", "Error closing stream", e);
                     }
                 }
+
+
             }
-            return null;
+        return null;
         }
 
         @Override
